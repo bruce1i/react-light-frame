2872125a89ad6474215d5adb877c021525778e52
Bruce <bruce.lixun@gmail.com>
2022-09-27 23:04:09 +0800

    Refactor: Test

--- src/utils/helpers/object.test.ts
+++ src/utils/helpers/object.test.ts
@@ -2,12 +2,12 @@ import { objectElseEmpty } from "./object";
 
 describe("objectElseEmpty", () => {
   it("will return object when the condition is true", () => {
-    expect(objectElseEmpty({ bar: "bar" }, true)).toEqual({ bar: "bar" });
-    expect(objectElseEmpty("bar", true)).toEqual({ bar: "" });
+    expect(objectElseEmpty({ bar: "bar" }, true)).toStrictEqual({ bar: "bar" });
+    expect(objectElseEmpty("bar", true)).toStrictEqual({ bar: "" });
   });
 
   it("will return empty object when the condition is false", () => {
-    expect(objectElseEmpty({ bar: "bar" }, false)).toEqual({});
-    expect(objectElseEmpty("bar", false)).toEqual({});
+    expect(objectElseEmpty({ bar: "bar" }, false)).toStrictEqual({});
+    expect(objectElseEmpty("bar", false)).toStrictEqual({});
   });
 });
