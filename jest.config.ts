ccebfb81ceae4a6a2d5fadd98969dd20eb807ed2
Bruce <bruce.lixun@gmail.com>
2022-09-29 18:33:50 +0800

    Refactor: Test

--- jest.config.ts
+++ jest.config.ts
@@ -3,10 +3,10 @@ import type { Config } from "jest";
 const config: Config = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["<rootDir>/test/__setups__/jest-dom-setup.ts"],
+  setupFilesAfterEnv: ["<rootDir>/tests/__setups__/jest-dom-setup.ts"],
   moduleNameMapper: {
     // mocks
-    "\\.(css|scss)$": "<rootDir>/test/__mocks__/style-mock.ts",
+    "\\.(css|scss)$": "<rootDir>/tests/__mocks__/style-mock.ts",
     // alias
     "@/(.*)": "<rootDir>/src/$1",
   },
