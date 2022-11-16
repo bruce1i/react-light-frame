0ad81043d9081ac55e17a1f4aff69068c8aede9a
Bruce <bruce.lixun@gmail.com>
2022-11-16 20:16:20 +0800

    Refactor: jest-css-modules-proxy

--- jest.config.ts
+++ jest.config.ts
@@ -3,13 +3,13 @@ import type { Config } from "jest";
 const config: Config = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["<rootDir>/tests/__setups__/jest-dom-setup.ts"],
-  snapshotResolver: "<rootDir>/tests/__resolvers__/snapshot-resolver.ts",
+  setupFilesAfterEnv: ["<rootDir>/tests/__configs__/jest-dom-setup.ts"],
+  snapshotResolver: "<rootDir>/tests/__configs__/snapshot-resolver.ts",
   moduleNameMapper: {
     // mocks
-    "\\.(css|scss)$": "<rootDir>/tests/__mocks__/style-mock.cjs",
+    "\\.(css|scss)$": "jest-css-modules-proxy",
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-      "<rootDir>/tests/__mocks__/file-mock.ts",
+      "<rootDir>/tests/__configs__/file-mock.ts",
     // alias
     "@/(.*)": "<rootDir>/src/$1",
     "rlf-test-utils": "<rootDir>/tests/__utils__/index",
